ae85089792bd84fcc5f6ad5c1baee9b6c483b8b8
Carlos Trejo <ctrejo@unitedvirtualities.com>
2019-12-11 06:59:00 -0700

    cambio

--- app/src/main/java/mx/edu/ittepic/tdm_u1_reva_practica/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/tdm_u1_reva_practica/MainActivity.kt
@@ -1,12 +1,17 @@
 package mx.edu.ittepic.tdm_u1_reva_practica
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.EditText
-import java.io.File
+import android.widget.Toast
+import java.io.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var btnSD : Button ?= null
     var btn : Button ?= null
     var nombre : EditText ?= null
     var telefono : EditText ?= null
@@ -14,10 +19,12 @@ class MainActivity : AppCompatActivity() {
     var HC : EditText ?= null
     var FC : EditText ?= null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnSD = findViewById(R.id.btnSD)
         btn = findViewById(R.id.btn)
         nombre = findViewById(R.id.nombre)
         telefono = findViewById(R.id.telefono)
@@ -25,15 +32,47 @@ class MainActivity : AppCompatActivity() {
         HC = findViewById(R.id.HC)
         FC = findViewById(R.id.FC)
 
+        val registro = nombre?.text.toString()+telefono?.text.toString()+medico?.text.toString()+HC?.text.toString()+FC?.text.toString()
 
 
-        var registro = Array<String>
-
-
+        btnSD?.setOnClickListener(){
+            try {
+                val file = File("Registro.txt")
+                val osw = OutputStreamWriter(FileOutputStream(file))
+                osw.write(registro)
+                osw.flush()
+                osw.close()
+                Toast.makeText(this, "Los datos fueron grabados correctamente", Toast.LENGTH_SHORT).show()
+                nombre?.setText("")
+                telefono?.setText("")
+                medico?.setText("")
+                HC?.setText("")
+                FC?.setText("")
+            } catch (ioe: IOException) {
+                Toast.makeText(this, "No se pudo grabar1", Toast.LENGTH_SHORT).show()
+            }
+        }
         btn?.setOnClickListener(){
-
-
+            try {
+                val file = File(Environment.getDataDirectory(),"Registro.txt")
+                val osw = OutputStreamWriter(FileOutputStream(file))
+                osw.write(registro)
+                osw.flush()
+                osw.close()
+                Toast.makeText(this, "Los datos fueron grabados correctamente", Toast.LENGTH_SHORT).show()
+                nombre?.setText("")
+                telefono?.setText("")
+                medico?.setText("")
+                HC?.setText("")
+                FC?.setText("")
+            } catch (ioe: IOException) {
+                Toast.makeText(this, "No se pudo grabar1", Toast.LENGTH_SHORT).show()
+            }
+        }
         }
 
+
     }
-}
+
+
+
